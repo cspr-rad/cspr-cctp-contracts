9701cee71b2e889ed0120fa9eaf0391f4061b5a6
jonas pauli <jonaspauli089@gmail.com>
2024-07-01 23:32:54 +0200

    implement BurnMessage no-std

--- src/message_transmitter.rs
+++ src/message_transmitter.rs
@@ -189,7 +189,7 @@ impl<'a> Message<'a>{
             // u32 size is 32 bits = 4 bytes
             self.data[index..(index + 4)]
                 .try_into()
-                .map_err(|_| Error)?
+                .unwrap()
         ))
     }
 
@@ -199,7 +199,7 @@ impl<'a> Message<'a>{
             // 64 size is 64 bits = 8 bytes
             self.data[index..(index + 8)]
                 .try_into()
-                .map_err(|_| Error)?
+                .unwrap()
         ))
     }
 
@@ -209,22 +209,109 @@ impl<'a> Message<'a>{
             // Pubkey size is 32 bytes
             &self.data[index..(index + 32)],
         )
-        .map_err(|_| Error)?)
+        .unwrap())
+    }
+}
+
+pub struct BurnMessage<'a> {
+    data: &'a [u8],
+}
+
+impl<'a> BurnMessage<'a> {
+    // Indices of each field in the message
+    const VERSION_INDEX: usize = 0;
+    const BURN_TOKEN_INDEX: usize = 4;
+    const MINT_RECIPIENT_INDEX: usize = 36;
+    const AMOUNT_INDEX: usize = 68;
+    const MSG_SENDER_INDEX: usize = 100;
+    // 4 byte version + 32 bytes burnToken + 32 bytes mintRecipient + 32 bytes amount + 32 bytes messageSender
+    const BURN_MESSAGE_LEN: usize = 132;
+    // EVM amount is 32 bytes while we use only 8 bytes on Solana
+    const AMOUNT_OFFSET: usize = 24;
+
+    /// Validates source array size and returns a new message
+    pub fn new(message_bytes: &'a [u8]) -> Self {
+        Self{
+            data: &message_bytes
+        }
     }
 
-
-    fn format_burn_message_body(
+    #[allow(clippy::too_many_arguments)]
+    /// Serializes given fields into a burn message
+    pub fn format_message(
         version: u32,
         burn_token: &Pubkey,
         mint_recipient: &Pubkey,
         amount: u64,
         message_sender: &Pubkey,
-    ) {
-        // todo: format burn message body
+    ) -> Vec<u8> {
+        let mut output: Vec<u8> = Vec::new();
+
+        output[Self::VERSION_INDEX..Self::BURN_TOKEN_INDEX].copy_from_slice(&version.to_be_bytes());
+        output[Self::BURN_TOKEN_INDEX..Self::MINT_RECIPIENT_INDEX]
+            .copy_from_slice(burn_token.as_ref());
+        output[Self::MINT_RECIPIENT_INDEX..Self::AMOUNT_INDEX]
+            .copy_from_slice(mint_recipient.as_ref());
+        output[(Self::AMOUNT_INDEX + Self::AMOUNT_OFFSET)..Self::MSG_SENDER_INDEX]
+            .copy_from_slice(&amount.to_be_bytes());
+        output[Self::MSG_SENDER_INDEX..Self::BURN_MESSAGE_LEN]
+            .copy_from_slice(message_sender.as_ref());
+
+        output
     }
-    // todo: replace message_hash with a keccak hash
-    fn verify_attestation_signatures(message_hash: &Vec<u8>, attestation: &Vec<u8>){
-        // todo: verify signatures using ECDSA, ED25519 or SECP256k1?
+
+    /// Returns version field
+    pub fn version(&self) -> u32 {
+        self.read_u32(Self::VERSION_INDEX).unwrap()
+    }
+
+    /// Returns burn_token field
+    pub fn burn_token(&self) -> Pubkey {
+        self.read_pubkey(Self::BURN_TOKEN_INDEX).unwrap()
+    }
+
+    /// Returns mint_recipient field
+    pub fn mint_recipient(&self) -> Pubkey {
+        self.read_pubkey(Self::MINT_RECIPIENT_INDEX).unwrap()
+    }
+
+    /// Returns amount field
+    pub fn amount(&self) -> u64 {
+        self.read_u64(Self::AMOUNT_INDEX + Self::AMOUNT_OFFSET).unwrap()
+    }
+
+    /// Returns message_sender field
+    pub fn message_sender(&self) -> Pubkey {
+        self.read_pubkey(Self::MSG_SENDER_INDEX).unwrap()
+    }
+
+    fn read_u32(&self, index: usize) -> Result<u32, Error>
+    {
+        Ok(u32::from_be_bytes(
+            // u32 size is 32 bits = 4 bytes
+            self.data[index..(index + 4)]
+                .try_into()
+                .unwrap()
+        ))
+    }
+
+    fn read_u64(&self, index: usize) -> Result<u64, Error>
+    {
+        Ok(u64::from_be_bytes(
+            // 64 size is 64 bits = 8 bytes
+            self.data[index..(index + 8)]
+                .try_into()
+                .unwrap()
+        ))
+    }
+
+    /// Reads pubkey field at the given offset
+    fn read_pubkey(&self, index: usize) -> Result<Pubkey, Error> {
+        Ok(Pubkey::try_from(
+            // Pubkey size is 32 bytes
+            &self.data[index..(index + 32)],
+        )
+        .unwrap())
     }
 }
 
